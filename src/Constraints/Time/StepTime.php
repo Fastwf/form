5e9dcf2c1f50ff1697679f2bf26ceb8b79960769
David BELBEZE <david.belbeze@gmail.com>
2022-01-23 17:41:08 +0100

    Add time step constraint

---
+++ src/Constraints/Time/StepTime.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Fastwf\Form\Constraints\Time;
+
+use Fastwf\Constraint\Api\Constraint;
+use Fastwf\Form\Exceptions\ValueError;
+use Fastwf\Form\Utils\DateIntervalUtil;
+
+/**
+ * Allows to validate the time using step in seconds.
+ */
+class StepTime implements Constraint
+{
+
+    /**
+     * The step to verify.
+     *
+     * @var double
+     */
+    protected $step;
+
+    /**
+     * The offset to use to validate the step.
+     *
+     * @var double
+     */
+    protected $from;
+
+    /**
+     * Constructor
+     *
+     * @param int|double $step the step in seconds 
+     * @param \DateInterval $from
+     */
+    public function __construct($step, $from = null)
+    {
+        if ($step === 0)
+        {
+            throw new ValueError("0 is not allowed");
+        }
+        else
+        {
+            $this->step = $step;
+        }
+
+        if ($from === null)
+        {
+            // By default the start is 0s
+            $this->from = new \DateInterval("P0D");
+        }
+        else
+        {
+            $this->from = $from;
+        }
+
+        $this->violationCode = $violationCode;
+    }
+
+    public function validate($node, $context)
+    {
+        $value = $node->get();
+
+        $quotient = (DateIntervalUtil::toSeconds($value)  - DateIntervalUtil::toSeconds($this->from)) / $this->step;
+
+        // Modulus cannot be used for double divider
+        return ((int) $quotient) == $quotient
+            ? null
+            : $context->violation($value, 'step-time', ['step' => $this->step]);
+    }
+
+}
---
+++ tests/Constraints/Time/StepTimeTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Fastwf\Tests\Constraints\Time;
+
+use Fastwf\Constraint\Data\Node;
+use Fastwf\Form\Exceptions\ValueError;
+use Fastwf\Form\Constraints\Time\StepTime;
+use Fastwf\Tests\Constraints\ConstraintTestCase;
+
+class StepTimeTest extends ConstraintTestCase
+{
+
+    /**
+     * @covers Fastwf\Form\Constraints\Time\StepTime
+     * @covers Fastwf\Form\Utils\DateIntervalUtil
+     */
+    public function testValidStep()
+    {
+        $constraint = new StepTime(60);
+        $node = Node::from(['value' => new \DateInterval("PT1H21M")]);
+
+        $this->assertNull($constraint->validate($node, $this->context));
+    }
+
+    /**
+     * @covers Fastwf\Form\Constraints\Time\StepTime
+     * @covers Fastwf\Form\Utils\DateIntervalUtil
+     */
+    public function testValidFrom()
+    {
+        $constraint = new StepTime(60, new \DateInterval("PT1H"));
+        $node = Node::from(['value' => new \DateInterval("PT1H21M")]);
+
+        $this->assertNull($constraint->validate($node, $this->context));
+    }
+
+    /**
+     * @covers Fastwf\Form\Constraints\Time\StepTime
+     * @covers Fastwf\Form\Utils\DateIntervalUtil
+     */
+    public function testInvalid()
+    {
+        $constraint = new StepTime(60, new \DateInterval("PT1H30S"));
+        $node = Node::from(['value' => new \DateInterval("PT1H21M")]);
+
+        $this->assertNotNull($constraint->validate($node, $this->context));
+    }
+
+    /**
+     * @covers Fastwf\Form\Constraints\Time\StepTime
+     * @covers Fastwf\Form\Utils\DateIntervalUtil
+     */
+    public function testConstructorException()
+    {
+        $this->expectException(ValueError::class);
+
+        $constraint = new StepTime(0);
+    }
+
+}
